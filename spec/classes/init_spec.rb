73e77258507e2b3938af263f232509fc1cf6dbc4
Roman Plessl <roman.plessl@nine.ch>
2015-11-04 00:50:00 +0100

    fix travis checks

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,12 +1,6 @@
 require 'spec_helper'
 
-describe 'telegraf::init' do
-   let(:pre_condition){
-      Package {
-        provider => 'apt',
-      }
-    }
-
+describe 'init' do
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_class('telegraf') }
 end
