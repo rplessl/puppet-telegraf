94b6d08bef8d42a05fe6e25eb57766cefc100947
Roman Plessl <roman.plessl@nine.ch>
2015-11-04 00:57:12 +0100

    improve tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
 
-describe 'init' do
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_class('telegraf') }
+describe 'telegraf' do
+  context 'supported operating systems' do
+     describe "telegraf class without any parameters on Ubuntu 14.04" do
+        let(:params) {{ }}
+        let(:facts) {{
+          :osfamily => 'debian',
+          :lsbdistid => 'ubuntu',
+          :lsbdistcodename => 'trusty',
+        }}
+
+        it { is_expected.to compile.with_all_deps }
+
+        it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
+        it { is_expected.to contain_class('telegraf::config') }
+        it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
+
+        it { is_expected.to contain_class('telegraf') }
+     end
+  end
 end
