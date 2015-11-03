643fc63a75d023ed94df1b36bf6723a478bd5b5c
Roman Plessl <roman.plessl@nine.ch>
2015-11-04 00:47:04 +0100

    added package provider

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe 'telegraf::init' do
+   let(:pre_condition){
+      Package {
+        provider => 'apt',
+      }
+    }
+
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_class('telegraf') }
 end
