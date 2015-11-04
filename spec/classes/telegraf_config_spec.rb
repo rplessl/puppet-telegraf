300ffa08e6c9c5ddf27fd235e03d585e0a6f39f2
Roman Plessl <roman.plessl@nine.ch>
2015-11-04 01:01:35 +0100

    fix rspec test

--- spec/classes/telegraf_config_spec.rb
+++ spec/classes/telegraf_config_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe 'telegraf::config', :type => :class do
+  context 'supported configurations' do
+    describe "telegraf class with pre_conditionals parameters on Ubuntu 14.04" do
+      let(:params) {{ }}
+      let(:facts) {{
+        :osfamily => 'debian',
+        :lsbdistid => 'ubuntu',
+        :lsbdistcodename => 'trusty',
+      }}
+      let(:pre_condition) {
+         'class{ "telegraf" :
+             install_from_repository => true,
+             config_base_file => "/etc/opt/telegraf/telegraf.conf"
+          }'
+      }
 
-  it { should create_class('telegraf::config') }
-
-  context 'with default preset variables' do
-    let(:pre_condition) {
-      'class{ "telegraf" :
-        install_from_repository => true,
-        config_base_file => "/etc/opt/telegraf/telegraf.conf"
-      }'
-    }
-
+      it { should create_class('telegraf::config') }
+    end
   end
 end
