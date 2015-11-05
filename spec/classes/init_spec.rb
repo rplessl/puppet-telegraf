26bedf418225e07b4e967c1c61a2e6bb49725070
Roman Plessl <roman@plessl.info>
2015-11-05 23:19:02 +0100

    added more tests (redhat, not supported omnios)

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,21 +2,49 @@ require 'spec_helper'
 
 describe 'telegraf' do
   context 'supported operating systems' do
-     describe "telegraf class without any parameters on Ubuntu 14.04" do
-        let(:params) {{ }}
-        let(:facts) {{
-          :osfamily => 'debian',
-          :lsbdistid => 'ubuntu',
-          :lsbdistcodename => 'trusty',
-        }}
+    describe "telegraf class without any parameters on Ubuntu 14.04" do
+      let(:params) {{ }}
+      let(:facts) {{
+        :osfamily => 'debian',
+        :lsbdistid => 'ubuntu',
+        :lsbdistcodename => 'trusty',
+      }}
 
-        it { is_expected.to compile.with_all_deps }
+      it { is_expected.to compile.with_all_deps }
 
-        it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
-        it { is_expected.to contain_class('telegraf::config') }
-        it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
 
-        it { is_expected.to contain_class('telegraf') }
-     end
+      it { is_expected.to contain_service('telegraf') }
+      it { is_expected.to contain_package('telegraf').with_ensure('installed') }
+      it { is_expected.to contain_class('telegraf') }
+    end
+    describe 'telegraf class without any parameters on Redhat/Centos' do
+      let(:params) {{ }}
+      let(:facts) {{
+        :osfamily => 'redhat',
+      }}
+
+      it { is_expected.to compile.with_all_deps }
+
+      it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
+
+      it { is_expected.to contain_service('telegraf') }
+      it { is_expected.to contain_package('telegraf').with_ensure('installed') }
+      it { is_expected.to contain_class('telegraf') }
+    end
+  end
+  context 'unsupported operating system' do
+    describe 'telegraf class without any parameters on Solaris/OmniOS' do
+      let(:facts) {{
+        :osfamily        => 'Solaris',
+        :operatingsystem => 'OmniOS',
+      }}
+
+      it { expect { is_expected.to contain_package('telegraf') }.to raise_error(Puppet::Error, /OS family Solaris not supported/) }
+    end
   end
 end
