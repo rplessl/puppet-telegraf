9958c4f403b128296b490e6a130f63ebf55f2224
Roman Plessl <roman@plessl.info>
2015-11-05 23:21:53 +0100

    fix test rule

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -44,7 +44,7 @@ describe 'telegraf' do
         :operatingsystem => 'OmniOS',
       }}
 
-      it { expect { is_expected.to contain_package('telegraf') }.to raise_error(Puppet::Error, /OS family Solaris not supported/) }
+      it { expect { is_expected.to contain_package('telegraf') }.to raise_error(Puppet::Error, /Only supports Debian \/ Ubuntu or RedHat and not/) }
     end
   end
 end
