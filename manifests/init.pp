471e7dadd1bbdcdfb0ffc5fc84dbe55d772ce900
Marin Vintila <marin.vintila@gmail.com>
2015-12-09 15:35:35 +0200

    Add configurable intervals and precision and ubuntu repository

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,8 +10,11 @@
 # [*version*]
 #  handle version of telegraf
 #
-# [*install_from_repository *]
+# [*install_from_repository*]
 #  Install telegraf from official repository
+#
+# [*manage_influx_repo*]
+#  Add the official Influx repository
 #
 # [*config_base_file*]
 #  path to the base configuration file
@@ -34,31 +37,44 @@
 # [*outputs_influxdb_password*]
 #  Password of output sink InfluxDB
 #
+# [*outputs_influxdb_precision*]
+#  Configures the influxdb output precision
+#
 # [*tags*]
 #  Tags given as a key / value pair slice
 #
 # [*agent_hostname*]
 #  Configures agent hostname for sending it to the sinks
 #
+# [*agent_collection_interval*]
+#  Configures agent collection interval
+#
+# [*agent_flush_interval*]
+#  Configures agent flush interval
+
 class telegraf (
   $ensure                    = 'installed',
   $version                   = '0.2.0',
   $install_from_repository   = true,
+  $manage_influx_repo        = false,
   $config_base_file          = '/etc/opt/telegraf/telegraf.conf',
   $config_directory          = '/etc/opt/telegraf/telegraf.d',
 
   # [outputs.influxdb] section of telegraf.conf
-  $outputs_influxdb_enabled  = true,
-  $outputs_influxdb_urls     = ['http://localhost:8086'],
-  $outputs_influxdb_database = 'telegraf',
-  $outputs_influxdb_username = 'telegraf',
-  $outputs_influxdb_password = 'metricsmetricsmetricsmetrics',
+  $outputs_influxdb_enabled   = true,
+  $outputs_influxdb_urls      = ['http://localhost:8086'],
+  $outputs_influxdb_database  = 'telegraf',
+  $outputs_influxdb_username  = 'telegraf',
+  $outputs_influxdb_password  = 'metricsmetricsmetricsmetrics',
+  $outputs_influxdb_precision = 's',
 
   # [tags] section of telegraf.conf
   $tags                      = undef,
 
   # [agent]
-  $agent_hostname            = 'localhost',
+  $agent_hostname            = $::hostname,
+  $agent_collection_interval = '10s',
+  $agent_flush_interval      = '10s',
 )
 {
   class { 'telegraf::install': }
