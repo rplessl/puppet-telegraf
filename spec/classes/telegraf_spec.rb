32382b599496d9ccc3c83ab968163b1abb77b962
Roman Plessl <roman.plessl@nine.ch>
2016-06-20 16:10:56 +0200

    enhancement for full featured test

--- spec/classes/telegraf_spec.rb
+++ spec/classes/telegraf_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe 'telegraf' do
 
-  context 'supported operating systems' do
+  context 'testing supported operating systems' do
 
     describe "testing telegraf class without any parameters on RedHat" do
       let(:params) {{ }}
       let(:facts) {{
-        :operatingsystem => 'redhat',
-        :osfamily => 'redhat',
+        :architecture              => 'amd64',
+        :operatingsystem           => 'redhat',
+        :operatingsystemmajrelease => '7',
+        :osfamily                  => 'redhat',
       }}
 
       it { is_expected.to compile.with_all_deps }
@@ -25,62 +27,87 @@ describe 'telegraf' do
       it { is_expected.to contain_class('telegraf') }
     end
 
-   ['Trusty', 'Xenial'].each do |lsbdistcodename|
-      describe "testing telegraf class without any parameters on #{lsbdistcodename}" do
-        let(:params) {{ }}
-        let(:facts) {{
-          :lsbdistcodename => lsbdistcodename,
-          :lsbdistid => 'ubuntu',
-          :operatingsystem => 'debian',
-          :osfamily => 'debian',
-        }}
+    describe "testing telegraf class without any parameters on Trusty" do
+      let(:params) {{ }}
+      let(:facts) {{
+        :architecture              => 'amd64',
+        :lsbdistcodename           => 'trusty',
+        :operatingsystemmajrelease => '14.04',
+        :lsbdistid                 => 'ubuntu',
+        :operatingsystem           => 'debian',
+        :osfamily                  => 'debian',
+      }}
 
-        it { is_expected.to compile.with_all_deps }
+      it { is_expected.to compile.with_all_deps }
 
-        it { is_expected.to contain_class('telegraf::params') }
-        it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
-        it { is_expected.to contain_class('telegraf::config') }
-        it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
-        it { is_expected.to contain_class('telegraf::cleanup') }
-        it { is_expected.to contain_class('telegraf::service').that_comes_before('telegraf::cleanup') }
+      it { is_expected.to contain_class('telegraf::params') }
+      it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::cleanup') }
+      it { is_expected.to contain_class('telegraf::service').that_comes_before('telegraf::cleanup') }
 
-        it { is_expected.to contain_service('telegraf') }
-        it { is_expected.to contain_package('telegraf').with_ensure('0.13.1') }
-        it { is_expected.to contain_class('telegraf') }
-      end
+      it { is_expected.to contain_service('telegraf') }
+      it { is_expected.to contain_package('telegraf').with_ensure('0.13.1') }
+      it { is_expected.to contain_class('telegraf') }
     end
 
-    ['Jessie'].each do |lsbdistcodename|
-      describe "testing telegraf class without any parameters on #{lsbdistcodename}" do
-        let(:params) {{ }}
-        let(:facts) {{
-          :lsbdistcodename => lsbdistcodename,
-          :lsbdistid => 'raspbian',
-          :operatingsystem => 'debian',
-          :osfamily => 'debian',
-        }}
+    describe "testing telegraf class without any parameters on Xenial" do
+      let(:params) {{ }}
+      let(:facts) {{
+        :architecture              => 'amd64',
+        :lsbdistcodename           => 'xenial',
+        :operatingsystemmajrelease => '16.04',
+        :lsbdistid                 => 'ubuntu',
+        :operatingsystem           => 'debian',
+        :osfamily                  => 'debian',
+      }}
 
-        it { is_expected.to compile.with_all_deps }
+      it { is_expected.to compile.with_all_deps }
 
-        it { is_expected.to contain_class('telegraf::params') }
-        it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
-        it { is_expected.to contain_class('telegraf::config') }
-        it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
-        it { is_expected.to contain_class('telegraf::cleanup') }
-        it { is_expected.to contain_class('telegraf::service').that_comes_before('telegraf::cleanup') }
+      it { is_expected.to contain_class('telegraf::params') }
+      it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::cleanup') }
+      it { is_expected.to contain_class('telegraf::service').that_comes_before('telegraf::cleanup') }
 
-        it { is_expected.to contain_service('telegraf') }
-        it { is_expected.to contain_package('telegraf').with_ensure('0.13.1') }
-        it { is_expected.to contain_class('telegraf') }
-      end
+      it { is_expected.to contain_service('telegraf') }
+      it { is_expected.to contain_package('telegraf').with_ensure('0.13.1') }
+      it { is_expected.to contain_class('telegraf') }
+    end
+
+    describe "testing telegraf class without any parameters on #{lsbdistcodename}" do
+      let(:params) {{ }}
+      let(:facts) {{
+        :architecture              => 'armv7l',
+        :lsbdistcodename           => 'Jessie',
+        :lsbdistid                 => 'raspbian',
+        :operatingsystem           => 'debian',
+        :operatingsystemmajrelease => '8',
+        :osfamily                  => 'debian',
+      }}
+
+      it { is_expected.to compile.with_all_deps }
+
+      it { is_expected.to contain_class('telegraf::params') }
+      it { is_expected.to contain_class('telegraf::install').that_comes_before('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::service').that_subscribes_to('telegraf::config') }
+      it { is_expected.to contain_class('telegraf::cleanup') }
+      it { is_expected.to contain_class('telegraf::service').that_comes_before('telegraf::cleanup') }
+
+      it { is_expected.to contain_service('telegraf') }
+      it { is_expected.to contain_package('telegraf').with_ensure('0.13.1') }
+      it { is_expected.to contain_class('telegraf') }
     end
   end
 
   context 'testing installation on unsupported operating system' do
     describe 'telegraf class without any parameters on Solaris' do
       let(:facts) {{
-        :osfamily => 'Solaris',
         :operatingsystem => 'Solaris',
+        :osfamily        => 'Solaris',
       }}
 
       it { expect { should contain_package('telegraf') }.to raise_error(Puppet::Error, /Telegraf not supported on Solaris/) }
@@ -90,10 +117,12 @@ describe 'telegraf' do
   context 'testing telegraf configuration file parameters' do
     let(:params) {{ }}
     let(:facts) {{
-      :osfamily => 'debian',
-      :operatingsystem => 'debian',
-      :lsbdistid => 'ubuntu',
-      :lsbdistcodename => 'trusty',
+        :architecture              => 'amd64',
+        :lsbdistcodename           => 'trusty',
+        :operatingsystemmajrelease => '14.04',
+        :lsbdistid                 => 'ubuntu',
+        :operatingsystem           => 'debian',
+        :osfamily                  => 'debian',
     }}
 
     it { is_expected.to contain_file('/etc/telegraf/telegraf.conf') }
@@ -103,10 +132,12 @@ describe 'telegraf' do
   context 'testing telegraf installation directory' do
     let(:params) {{ }}
     let(:facts) {{
-      :osfamily => 'debian',
-      :operatingsystem => 'debian',
-      :lsbdistid => 'ubuntu',
-      :lsbdistcodename => 'trusty',
+        :architecture              => 'amd64',
+        :lsbdistcodename           => 'trusty',
+        :operatingsystemmajrelease => '14.04',
+        :lsbdistid                 => 'ubuntu',
+        :operatingsystem           => 'debian',
+        :osfamily                  => 'debian',
     }}
 
     it { is_expected.to contain_file('/opt/telegraf') }
